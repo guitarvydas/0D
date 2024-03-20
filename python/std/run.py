f5e5b9c3b0f915821cb0ddb92d5a989498e073c0
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-03-20 15:32:22 -0400

    run.py

--- python/std/run.py
+++ python/std/run.py
@@ -1,70 +1,37 @@
-???
-// run prints only the output on port "output", whereas run_demo prints all outputs
-run :: proc (r : ^zd.Component_Registry, arg: string, main_container_name : string, diagram_source_files : [dynamic]string, injectfn : #type proc (string, ^zd.Eh)) {
-    pregistry := r
-    // get entrypoint container
-    main_container, ok := zd.get_component_instance(pregistry, main_container_name, owner=nil)
-    fmt.assertf(
-        ok,
-        "Couldn't find container with page name %s in files %s (check tab names, or disable compression?)\n",
-        main_container_name,
-        diagram_source_files,
-    )
+# run prints only the output on port "output", whereas run_demo prints all outputs
+def run (pregistry, arg, main_container_name, diagram_source_files, injectfn):
+    # get entrypoint container
+    main_container = get_component_instance(pregistry, main_container_name, owner=nil)
+    if None == main_container:
+        load_error (f"Couldn't find container with page name {main_container_name} in files {diagram_source_files} (check tab names, or disable compression?)")
     injectfn (arg, main_container)
     print_error_maybe (main_container)
     print_output (main_container)
-}
 
-run_all_outputs :: proc (r : ^zd.Component_Registry, arg: string, main_container_name : string, diagram_source_files : [dynamic]string, injectfn : #type proc (string, ^zd.Eh)) {
-    pregistry := r
-    // get entrypoint container
-    main_container, ok := zd.get_component_instance(pregistry, main_container_name, owner=nil)
-    fmt.assertf(
-        ok,
-        "Couldn't find container with page name %s in files %s (check tab names, or disable compression?)\n",
-        main_container_name,
-        diagram_source_files,
-    )
+def run_all_outputs (pregistry, arg, main_container_name, diagram_source_files, injectfn):
+    # get entrypoint container
+    main_container = get_component_instance(pregistry, main_container_name, owner=nil)
+    if None == main_container:
+        load_error (f"Couldn't find container with page name {main_container_name} in files {diagram_source_files} (check tab names, or disable compression?)")
     injectfn (arg, main_container)
     print_error_maybe (main_container)
     dump_outputs (main_container)
-}
 
-run_demo :: proc (r : ^zd.Component_Registry, arg, main_container_name : string, diagram_source_files : [dynamic]string, injectfn : #type proc (string, ^zd.Eh)) {
-    pregistry := r
-    // get entrypoint container
-    main_container, ok := zd.get_component_instance(pregistry, main_container_name, owner=nil)
-    fmt.assertf(
-        ok,
-        "Couldn't find container with page name %s in files %s (check tab names, or disable compression?)\n",
-        main_container_name,
-        diagram_source_files,
-    )
+def run_demo (pregistry, arg, main_container_name, diagram_source_files, injectfn):
+    # get entrypoint container
+    main_container = get_component_instance(pregistry, main_container_name, owner=nil)
+    if None == main_container:
+        load_error (f"Couldn't find container with page name {main_container_name} in files {diagram_source_files} (check tab names, or disable compression?)")
     injectfn (arg, main_container)
     dump_outputs (main_container)
-    fmt.println("\n\n--- done ---")
-}
+    print ("--- done ---")
 
-
-
-run_demo_debug :: proc (r : ^zd.Component_Registry, arg: string, main_container_name : string, diagram_source_files : [dynamic]string, injectfn : #type proc (string, ^zd.Eh)) {
-    pregistry := r
-    // get entrypoint container
-    main_container, ok := zd.get_component_instance(pregistry, main_container_name, owner=nil)
-    fmt.assertf(
-        ok,
-        "Couldn't find main container with page name %s in files %s (check tab names, or disable compression?)\n",
-        main_container_name,
-        diagram_source_files,
-    )
-
-    dump_hierarchy (main_container)
-
+def run_demo_debug (pregistry, arg, main_container_name, diagram_source_files, injectfn):
+    # get entrypoint container
+    main_container = get_component_instance(pregistry, main_container_name, owner=nil)
+    if None == main_container:
+        load_error (f"Couldn't find container with page name {main_container_name} in files {diagram_source_files} (check tab names, or disable compression?)")
+    dump_hierarchy (main_controller)
     injectfn (arg, main_container)
-
     dump_outputs (main_container)
-    dump_stats (pregistry)
-
-    fmt.println("\n\n--- done ---")
-}
-
+    print ("--- done ---")
