37d4e83362d6508779e6cb16d13ab59a20f46e4b
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-03-20 15:57:06 -0400

    transpiler.py

---
+++ python/std/transpiler.py
@@ -0,0 +1,47 @@
+class OhmJS_Instance_Data:
+    def _init_ (self):
+        self.grammarname = None
+        self.grammarfilename = None
+        self.semanticsfilename = None
+        self.s = None
+
+def ohmjs_instantiate (name, owner):
+    instance_name = gensym ("OhmJS")
+    inst = OhmJS_Instance_Data () # all fields have zero value before any messages are received
+    return make_leaf (instance_name, owner, inst, ohmjs_handle)
+
+def ohmjs_maybe (eh, inst, causingMsg):
+    if None != inst.grammarname and None != inst.grammarfilename and None != inst.semanticsfilename and None != inst.s:
+        cmd = "0d/python/std/ohmjs.js {inst.grammarname} {inst.grammarfilename} {inst.semanticsfilename}"
+        captured_output = run_command (cmd, inst.s)
+
+        errstring = trimws (err)
+        if len (errstring) == 0:
+            zd.send_string (eh, "", strings.trimws (captured_output), causingMsg)
+        else:
+            zd.send_string (eh, "✗", errstring, causingMsg)
+        inst.grammarName = None
+        inst.grammarfilename = None
+        inst.semanticsfilename = None
+        self.s = None
+
+def ohmjs_handle (eh, msg):
+    inst = eh.instance_data
+    if msg.port == "grammar name":
+        inst.grammarname = clone_string (msg.datum.srepr ())
+        ohmjs_maybe (eh, inst, msg)
+    elif msg.port == "grammar":
+        inst.grammarfilename = clone_string (msg.datum.repr ())
+        ohmjs_maybe (eh, inst, msg)
+    elif msg.port == "semantics":
+        inst.semanticsfilename = clone_string (msg.datum.repr ())
+        ohmjs_maybe (eh, inst, msg)
+    elif msg.port == "input":
+        inst.s = clone_string (msg.datum.repr ())
+        ohmjs_maybe (eh, inst, msg)
+    else:
+        emsg = f"!!! ERROR: OhmJS got an illegal message port {msg.port}"
+        send_string (eh, "✗", emsg, msg)
+
+
+
