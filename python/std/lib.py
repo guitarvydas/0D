38ac1faeb06b0076d6a1c17f1c773b34d37ac971
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-03-20 15:21:52 -0400

    lib.py

---
+++ python/std/lib.py
@@ -0,0 +1,56 @@
+import sys
+
+# usage: app arg main diagram_filename1 diagram_filename2 ...
+def parse_command_line_args ():
+    # return a 3-element array [arg, main_container_name, [diagram_names]]
+    if (len (sys.argv) < (3+1)):
+        load_error ("usage: app <arg> <main tab name> <diagram file name 1> ...")
+        return None
+    else:
+        arg = sys.argv [1]
+        main_container_name = sys.argv [2]
+        diagram_source_files = sys.argv [3:]
+        return [arg, main_container_name, diagram_source_files]
+
+def initialize_component_palette (diagram_source_files, project_specific_components_subroutine):
+    reg = make_component_registry ()
+    for diagram_source in diagram_source_files:
+        collect_process_leaves (reg, diagram_source)
+        all_containers_within_single_file = json2internal (diagram_source)
+        for container in all_containers_within_single_file:
+            register_component (reg, Template (name=container.name , template_data=container, instantiator=container_instantiator))
+    initialize_stock_components (reg)
+    project_specific_components (reg) # add user specified components (probably only leaves)
+    return reg
+
+
+def print_error_maybe (main_container):
+    error_port = "✗"
+    err = fetch_first_output (main_container, error_port)
+    if found and (0 < len (trimws (err))):
+        print ("--- !!! ERRORS !!! ---")
+        print_specific_output (main_container, error_port, false)
+
+
+# debugging helpers
+
+def dump_outputs (main_container):
+    print ()
+    print ("--- Outputs ---")
+    print_output_list (main_container)
+
+def dump_hierarchy (main_container):
+    print ()
+    print ("--- Hierarchy ---")
+    print (build_hierarchy (main_container))
+
+def build_hierarchy (c):
+    s = ""
+    for child in c.children:
+        s = f"{s}{build_hierarchy (child)}"
+    return f"\n({c.name}{s})"
+
+#
+def trimws (s):
+    # remove whitespace from front and back of string
+    return s.strip ()
---
+++ python/std/run.py
@@ -0,0 +1,70 @@
+???
+// run prints only the output on port "output", whereas run_demo prints all outputs
+run :: proc (r : ^zd.Component_Registry, arg: string, main_container_name : string, diagram_source_files : [dynamic]string, injectfn : #type proc (string, ^zd.Eh)) {
+    pregistry := r
+    // get entrypoint container
+    main_container, ok := zd.get_component_instance(pregistry, main_container_name, owner=nil)
+    fmt.assertf(
+        ok,
+        "Couldn't find container with page name %s in files %s (check tab names, or disable compression?)\n",
+        main_container_name,
+        diagram_source_files,
+    )
+    injectfn (arg, main_container)
+    print_error_maybe (main_container)
+    print_output (main_container)
+}
+
+run_all_outputs :: proc (r : ^zd.Component_Registry, arg: string, main_container_name : string, diagram_source_files : [dynamic]string, injectfn : #type proc (string, ^zd.Eh)) {
+    pregistry := r
+    // get entrypoint container
+    main_container, ok := zd.get_component_instance(pregistry, main_container_name, owner=nil)
+    fmt.assertf(
+        ok,
+        "Couldn't find container with page name %s in files %s (check tab names, or disable compression?)\n",
+        main_container_name,
+        diagram_source_files,
+    )
+    injectfn (arg, main_container)
+    print_error_maybe (main_container)
+    dump_outputs (main_container)
+}
+
+run_demo :: proc (r : ^zd.Component_Registry, arg, main_container_name : string, diagram_source_files : [dynamic]string, injectfn : #type proc (string, ^zd.Eh)) {
+    pregistry := r
+    // get entrypoint container
+    main_container, ok := zd.get_component_instance(pregistry, main_container_name, owner=nil)
+    fmt.assertf(
+        ok,
+        "Couldn't find container with page name %s in files %s (check tab names, or disable compression?)\n",
+        main_container_name,
+        diagram_source_files,
+    )
+    injectfn (arg, main_container)
+    dump_outputs (main_container)
+    fmt.println("\n\n--- done ---")
+}
+
+
+
+run_demo_debug :: proc (r : ^zd.Component_Registry, arg: string, main_container_name : string, diagram_source_files : [dynamic]string, injectfn : #type proc (string, ^zd.Eh)) {
+    pregistry := r
+    // get entrypoint container
+    main_container, ok := zd.get_component_instance(pregistry, main_container_name, owner=nil)
+    fmt.assertf(
+        ok,
+        "Couldn't find main container with page name %s in files %s (check tab names, or disable compression?)\n",
+        main_container_name,
+        diagram_source_files,
+    )
+
+    dump_hierarchy (main_container)
+
+    injectfn (arg, main_container)
+
+    dump_outputs (main_container)
+    dump_stats (pregistry)
+
+    fmt.println("\n\n--- done ---")
+}
+
