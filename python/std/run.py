85804bade6b38d0de208f38ed91b532860c72938
Paul Tarvydas <paul.tarvydas@gmail.com>
2024-04-02 20:51:41 -0400

    remove run_all_outputs and run_demo_debug

--- python/std/run.py
+++ python/std/run.py
@@ -10,17 +10,6 @@ def run (pregistry,  root_project, root_0D, arg, main_container_name, diagram_so
     print_error_maybe (main_container)
     dump_outputs (main_container)
 
-def run_all_outputs (pregistry,  root_project, root_0D, arg, main_container_name, diagram_source_files, injectfn):
-    set_environment (root_project, root_0D)
-    # get entrypoint container
-    main_container = get_component_instance(pregistry, main_container_name, owner=None)
-    if None == main_container:
-        load_error (f"Couldn't find container with page name {main_container_name} in files {diagram_source_files} (check tab names, or disable compression?)")
-    if not load_errors:
-        injectfn (root_project, root_0D, arg, main_container)
-    print_error_maybe (main_container)
-    dump_outputs (main_container)
-
 def run_demo (pregistry, root_project, root_0D, arg, main_container_name, diagram_source_files, injectfn,
               show_hierarchy=True, show_connections=True, show_traces=True):
     set_environment (root_project, root_0D)
@@ -28,26 +17,15 @@ def run_demo (pregistry, root_project, root_0D, arg, main_container_name, diagra
     main_container = get_component_instance(pregistry, main_container_name, owner=None)
     if None == main_container:
         load_error (f"Couldn't find container with page name {main_container_name} in files {diagram_source_files} (check tab names, or disable compression?)")
-    if not load_errors:
-        injectfn (root_project, root_0D, arg, main_container)
     if show_hierarchy:
         dump_hierarchy (main_container)
     if show_connections:
         dump_connections (main_container)
+    if not load_errors:
+        injectfn (root_project, root_0D, arg, main_container)
     dump_outputs (main_container)
     if show_traces:
         print ("--- routing traces ---")
         print (routing_trace_all (main_container))
     print ("--- done ---")
 
-def run_demo_debug (pregistry, root_project, root_0D, arg, main_container_name, diagram_source_files, injectfn):
-    set_environment (root_project, root_0D)
-    # get entrypoint container
-    main_container = get_component_instance(pregistry, main_container_name, owner=None)
-    if None == main_container:
-        load_error (f"Couldn't find container with page name {main_container_name} in files {diagram_source_files} (check tab names, or disable compression?)")
-    dump_hierarchy (main_container)
-    if not load_errors:
-        injectfn (root_project, root_0D, arg, main_container)
-    dump_outputs (main_container)
-    print ("--- done ---")
